3cf990ebcaedd22193bd3c6fd9f3e3e485d10a28
flo <flo.liang@my.com>
2015-02-18 22:20:54 -0800

    fixing views

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     address:         'smtp.sendgrid.net',
     port:            '587',
     authentication:  :plain,
-    user_name:       ENV['app33653201@heroku.com'],
-    password:        ENV['huh9205i'],
+    user_name:       ENV['SENDGRID_USERNAME'],
+    password:        ENV['SENDGRID_PASSWORD'],
     domain:          'heroku.com',
     enable_starttls_auto: true
   }
