dd74b596fdbe0cbb5fec6a821aef1c81f960377d
flo <flo.liang@my.com>
2015-02-25 20:00:16 -0800

    Complete Authorization assignment

--- db/migrate/20150220053014_add_user_toposts.rb
+++ db/migrate/20150220053014_add_user_toposts.rb
@@ -1,6 +1,7 @@
 class AddUserToposts < ActiveRecord::Migration
   def change
     add_column :posts, :user_id, :integer
+    add_column :posts, :published
     add_index :posts, :user_id
   end
 end
