297abee11553eb6e794853a116092aea8d19ce25
flo <flo.liang@my.com>
2015-02-25 23:48:05 -0800

    Associate and nests topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,21 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    @posts = policy_scope(Post)
-     authorize @posts
-  end
 
   def show
+    @topic = Topic.find(params[:topic_id])
       @post = Post.find(params[:id])
   end
 
   def new
-     @post = Post.new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
        authorize @post
   end
 
   def create
-    
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
         authorize @post
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -28,11 +26,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+      @topic = Topic.find(params[:topic_id])
       @post = Post.find(params[:id])
       authorize @post
   end
 
   def update
+     @topic = Topic.find(params[:topic_id])
       @post = Post.find(params[:id])
         authorize @post
       if @post.update_attributes(params.require(:post).permit(:title, :body))
