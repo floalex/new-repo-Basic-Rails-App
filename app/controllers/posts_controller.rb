78dabd84103aac41d5fcc5e2f7bb7eb3d9274903
flo <flo.liang@my.com>
2015-02-27 22:50:38 -0800

    Complete Interlude checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,13 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
         authorize @post
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -33,14 +34,20 @@ class PostsController < ApplicationController
 
   def update
      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
         authorize @post
-      if @post.update_attributes(params.require(:post).permit(:title, :body))
+      if @post.update_attributes(post_params)
         flash[:notice] = "Post was upated."
-        redirect_to @post
+        redirect_to [@topic, @post]
       else
         flash[:error] = "There was an error saving a post, please try again."
-        render :edit
+        render :new
       end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
