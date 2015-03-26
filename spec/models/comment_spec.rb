a8ea728d23b062ee92fef848327f895c380ad907
flo <flo.liang@my.com>
2015-03-25 19:39:52 -0700

    Complete the whole favoriting checkpoint

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,8 +8,8 @@ describe Comment do
 
 
   before do
-    @post = associated_post
     @user = authenticated_user
+    @post = associated_post
     @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
   end
 
@@ -25,8 +25,7 @@ describe Comment do
   end
 
   it "does not send emails to users who haven't favorited the post" do
-    expect ( FavoriteMailer )
-      .not_to receive(:new_comment)
+    expect( FavoriteMailer ).not_to receive(:new_comment)
 
     @comment.save
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,32 @@ describe User do
   include TestFactories
 
   describe '#favorited(post)' do
-    xit "returns 'nil' if the user has not favorited the post" do
+
+    before do
+    @user = authenticated_user
+    @post = associated_post
+    end
+    
+    it "returns 'nil' if the user has not favorited the post" do
+      expect( @user.favorited(@post) ).to eq(nil)
     end
   
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorited(@post) ).to eq(favorite) 
+    end
+  
+    
+    it "returns nil if the user has favorited another post " do
+      @another_post = Post.create(
+                      user: @user, 
+                      body: "Another body.",
+                      title: "Another title.",
+                      topic: Topic.create(name: 'Topic name'),
+                      )
+      
+      favorite = @user.favorites.create(post: @post)
+      expect( @user.favorited(@another_post) ).to eq(nil)
     end
   end
-  
 end
